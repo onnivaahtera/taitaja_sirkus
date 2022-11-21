a483bc272983794402a30d94e0d9a375e5f30328
onnivaahtera <onni.vaahtera@gmail.com>
2022-11-21 13:41:46 +0200

    tailwind and logo

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react'
+import logo from '../img/logo.jpg'
 
-interface headerProps {
-  
-}
 
-const Header: FC<headerProps> = ({}) => {
+const Header = () => {
   return (
-    <div>
-        Header
+    <div className='bg-amber-800 h-14 flex'>
+      <div>
+        <a href="/">
+          <img className='w-40 h-14' src={logo} alt="" />
+        </a>
+      </div>
+        <a href="/about">Sirkunnan toimintaa</a>
     </div>
   )
 }
