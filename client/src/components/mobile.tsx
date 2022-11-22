ca60bafeed0e95b62aeca7cbdc60e3f73d39d896
onnivaahtera <onni.vaahtera@gmail.com>
2022-11-22 11:39:45 +0200

    Mobile menu done

--- client/src/components/mobile.tsx
+++ client/src/components/mobile.tsx
@@ -3,40 +3,33 @@ import menu from "../img/menu_large.png";
 import close from "../img/close.png";
 
 function Moblie() {
-  function openNav() {
+  function Nav() {
     const btn = document.getElementById("nav") as HTMLButtonElement;
-    btn.style.width = "100%";
-  }
-
-  function closeNav() {
-    const btn = document.getElementById("nav") as HTMLButtonElement;
-    btn.style.width = "0%";
+    if (btn.style.height === "100%") {
+      btn.style.height = "0%";
+    } else {
+      btn.style.height = "100%";
+    }
   }
 
   return (
     <>
       <div className="block md:hidden">
-        <div className="absolute right-0 m-2">
-          <button onClick={openNav}>
+        <div className="absolute right-0 m-2 z-50">
+          <button onClick={Nav}>
             <img src={menu} alt="" />
           </button>
         </div>
 
         <div
           id="nav"
-          className="h-full w-0 fixed z-10 left-0 top-0 overflow-x-auto transition-all bg-overlay"
+          className="h-0 w-full fixed z-10 left-0 top-0 overflow-x-auto duration-300 bg-overlay"
         >
-          <div className="absolute top-0 right-0">
-            <button onClick={closeNav}>
-              <img src={close} alt="" />
-            </button>
-          </div>
-
-          <div className="relative top-5 w-full text-center mt-8">
-            <div>
+          <div className="relative text-2xl top-5 w-full text-center mt-8">
+            <div className="p-2">
               <a href="/">Etusivu</a>
             </div>
-            <div>
+            <div className="p-2">
               <a href="/about">Sirkunnan toimintaa</a>
             </div>
           </div>
