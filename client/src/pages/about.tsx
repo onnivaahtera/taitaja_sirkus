83ca6bf939ca9b739b1a34ae6d867fcea99b8511
onnivaahtera <onni.vaahtera@gmail.com>
2022-11-22 11:40:12 +0200

    about page text layout

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -5,16 +5,21 @@ interface aboutProps {}
 const About: FC<aboutProps> = ({}) => {
   return (
     <>
-      <div className="m-4 p-2">
-        Sirikus on pieni sirkus, joka voi järjestää esityksensä missä tahansa.
-        Esitys voi olla isossa teatterissa tai vaikka omassa kodissasi.
-        Katsojamäärät vaihtelevat muutamasta ihmisestä jopa satoihin. Esitysten
-        teema voi vaihdella asiakkaan toiveiden mukaan. Teemat vaihtelevat
-        rauhallisesta ja eteerisestä jooga teemasta vaikka räjähtävään avaruuden
-        valloitukseen. Sirkus Sirikuksessa työskentelee 5 innokasta ja taitavaa
-        henkilöä. He vastaavat kaikesta esitysten vaatimista järjestelyistä.
-        Sirkuksella on käytössään oma pakettiauto jonka kyydissä kulkee kaikki
-        esityksessä tarvittava rekvisiitta.
+      <div className="mx-6">
+        <div className="m-4 p-2">
+          Sirikus on pieni sirkus, joka voi järjestää esityksensä missä tahansa.
+          Esitys voi olla isossa teatterissa tai vaikka omassa kodissasi.
+          Katsojamäärät vaihtelevat muutamasta ihmisestä jopa satoihin.
+          Esitysten teema voi vaihdella asiakkaan toiveiden mukaan. Teemat
+          vaihtelevat rauhallisesta ja eteerisestä jooga teemasta vaikka
+          räjähtävään avaruuden valloitukseen.
+        </div>
+        <div className="m-4 p-2">
+          Sirkus Sirikuksessa työskentelee 5 innokasta ja taitavaa henkilöä. He
+          vastaavat kaikesta esitysten vaatimista järjestelyistä. Sirkuksella on
+          käytössään oma pakettiauto jonka kyydissä kulkee kaikki esityksessä
+          tarvittava rekvisiitta.
+        </div>
       </div>
     </>
   );
