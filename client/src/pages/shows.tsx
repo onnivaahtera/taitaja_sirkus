1a9eba9241abf7da82bea7cdb6d8d1169551bf1c
onnivaahtera <onni.vaahtera@gmail.com>
2022-11-22 18:22:24 +0200

    show state

--- client/src/pages/shows.tsx
+++ client/src/pages/shows.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 
 function Shows() {
-  const [data, setData] = useState<any[]>();
+  const [show, setShow] = useState();
 
   useEffect(() => {
     fetch("http://localhost/taitaja_sirkus/php/api/read.php")
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => setShow(data?.body));
   }, []);
 
-  console.log(data);
-
-  return <div>Shows</div>;
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 }
 
 export default Shows;
