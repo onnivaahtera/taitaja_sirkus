c94bc0461d7ccfe4b969bebe7398e105085068d2
onnivaahtera <onni.vaahtera@gmail.com>
2022-11-23 12:54:14 +0200

    Show cards

--- client/src/pages/shows.tsx
+++ client/src/pages/shows.tsx
@@ -1,17 +1,44 @@
 import React, { useEffect, useState } from "react";
 
+type show = {
+  id: number;
+  teema: string;
+  esityspaikka: string;
+  kaupunki: string;
+  pvm: Date;
+  paikata: number;
+  vapaitapaikkoja: number;
+};
+
 function Shows() {
-  const [show, setShow] = useState();
+  const [show, setShow] = useState<any[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost/taitaja_sirkus/php/api/read.php")
+    fetch("http://localhost/sirkus/php/api/read.php")
       .then((res) => res.json())
       .then((data) => setShow(data?.body));
   }, []);
 
+  if (!show) return <div>No shows</div>;
+
   return (
     <div>
-      <div></div>
+      <div>
+        <ul>
+          {show.map((item, key) => (
+            <button
+              disabled={item.freeSeats <= 0}
+              onClick={() => (window.location.href = "/")}
+              key={key}
+              className="m-3 p-3 bg-header w-fit"
+            >
+              <li>Teema: {item.theme}</li>
+              <li>Paikkoja{item.seats}</li>
+              <li>Vapaita paikkoja: {item.freeSeats}</li>
+            </button>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
