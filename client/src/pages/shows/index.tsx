635fb742586ef5e45cfacfd8b01bb806743f420a
onnivaahtera <onni.vaahtera@gmail.com>
2022-11-23 13:27:25 +0200

    ticket-form

--- client/src/pages/shows/index.tsx
+++ client/src/pages/shows/index.tsx
@@ -1,15 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-type show = {
-  id: number;
-  teema: string;
-  esityspaikka: string;
-  kaupunki: string;
-  pvm: Date;
-  paikata: number;
-  vapaitapaikkoja: number;
-};
-
 function Shows() {
   const [show, setShow] = useState<any[]>([]);
 
@@ -28,13 +18,13 @@ function Shows() {
           {show.map((item, key) => (
             <button
               disabled={item.freeSeats <= 0}
-              onClick={() => (window.location.href = "/")}
+              onClick={() => (window.location.href = `./show/${item.id}`)}
               key={key}
               className="m-3 p-3 bg-header w-fit"
             >
-              <li>Teema: {item.theme}</li>
-              <li>Paikkoja: {item.seats}</li>
-              <li>Vapaita paikkoja: {item.freeSeats}</li>
+              <p>Teema: {item.theme}</p>
+              <p>Paikkoja: {item.seats}</p>
+              <p>Vapaita paikkoja: {item.freeSeats}</p>
             </button>
           ))}
         </ul>
