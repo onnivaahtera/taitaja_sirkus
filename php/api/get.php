86a0ae29759c6715918b1facb025b1c4acc4d240
onnivaahtera <onni.vaahtera@gmail.com>
2022-12-01 10:59:14 +0200

    ticket verification

--- php/api/get.php
+++ php/api/get.php
@@ -1,5 +1,32 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/database.php';
+include_once '../class/shows.php';
+$database = new Database();
+$db = $database->getConnection();
+$items = new Shows($db);
+$stmt = $items->getShows();
+$itemCount = $stmt->rowCount();
 
 $data = json_decode(file_get_contents('php://input'), true);
-print_r($data);
-echo $data["operacion"];
+$parsedData = $data['order'];
+
+
+if ($itemCount > 0) {
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        if ($vapaitapaikkoja < $parsedData['tickets']) {
+            echo json_encode(
+                array("message" => "Ei tarpeeksi vapaita paikkoja")
+            );
+        }
+    }
+} else {
+    http_response_code(404);
+    echo json_encode(
+        array("message" => "No record found.")
+    );
+}
---
+++ php/class/customer.php
@@ -0,0 +1,17 @@
+<?php
+
+class Customer
+{
+    // connection
+    private $conn;
+
+    // database table
+    private $table = "tilaaja";
+
+    // Database columns
+
+    public function __construct($db)
+    {
+        $this->conn;
+    }
+}
